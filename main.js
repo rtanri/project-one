2b2f968d1d0f4c716478989dce2562ea802fe8bb
Revin Tanri <tanrirevin@gmail.com>
2021-04-20 20:13:39 +0800

    Created dummy 6 <div> for tower ground location mouse hover

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Helo world")
+// Pseudo code
+// 1. Create object class type: tower, enemy, and bullet
+// 2. test to summon the object in canvas in window.onload. if success, hide it first
+
+// 3. function to set tower by clicking button, and click to set tower position
+
+// 4. Create path for enemy
+
+// 5. Create randomizer to summon the enemy position (row 1, row2, row 3)
+
+// 6. Create collision function
+
+// 7. Set if enemy Health is Zero, add GOLD amount
